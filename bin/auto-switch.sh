abb51038c23e584a4faeb737eca2e931a4373ea6
g-hyoga <hyoga0216@gmail.com>
2018-04-14 14:10:17 +0900

    '' -> "" :angel:

--- bin/auto-switch.sh
+++ bin/auto-switch.sh
@@ -8,10 +8,11 @@ if [ -z "$LAB_SSID" ]; then
   LAB_SSID="KE-101-1"
 fi
 
-/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -s | grep '$LAB_SSID'
-if [ $? != 0 ]; then
+/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -s | grep "$LAB_SSID"
+if [ "$?" == 0 ]; then
   scselect $LAB_NETWORK
 else 
   scselect Automatic
 fi
+
 
